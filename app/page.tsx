f0be848b440a34eb685449e4f94b0ad37c35927e
kevinmutai <mutaikevinkip.05@gmail.com>
2025-06-10 07:59:51 +0300

    feat: home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,42 @@
-import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import React from 'react'
 
 const Page = () => {
   return (
     <div>
-      <h1 className='text-2xl underline'>Welcome to My SAAS App</h1>
-      <Button>Let's get started</Button>
+      <h1 className='text-2xl'>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard
+          id="123"
+          name="Neura the brainy explorer"
+          topic="Neural network of the brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+           id="456"
+           name="Countsy the number wizard"
+           topic="Derivatives and integrals"
+           subject="maths"
+           duration={30}
+           color="#e5d0ff"
+        />
+        <CompanionCard
+           id="789"
+           name="Verba the vocabulary builder"
+           topic="Language"
+           subject="English literature"
+           duration={30}
+           color="#bde7ff"
+        />
+      </section>
+      <section className='home-section'>
+        <CompanionsList/>
+        <CTA/>
+      </section>
     </div>
   )
 }
